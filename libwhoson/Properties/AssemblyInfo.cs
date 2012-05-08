cdd0a75570dc0945dc02a01136ecb52dbe4a7fbc
Anders Lövgren <andlov@nowise.se>
2012-05-08 22:51:42 +0000

    Incremented version information. Fixed copyright notice.

--- libwhoson/Properties/AssemblyInfo.cs
+++ libwhoson/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Uppsala University")]
 [assembly: AssemblyProduct("libwhoson")]
-[assembly: AssemblyCopyright("Copyright © 2011 Anders Lövgren (QNET/Compdept BMC)")]
+[assembly: AssemblyCopyright("Copyright © 2011-2012 Anders Lövgren (QNET/Compdept BMC)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
